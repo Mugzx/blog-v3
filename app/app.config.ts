dad250bcb0ee2cbab9d6300af2b6eeb7b1093617
Mugzx <158086960+Mugzx@users.noreply.github.com>
2025-10-31 02:41:23 +0800

    chore: 隐藏 `CommGroup` 组件, 移除群聊链接

--- app/app.config.ts
+++ app/app.config.ts
@@ -44,7 +44,6 @@ export default defineAppConfig({
 		/** 侧边栏底部图标导航 */
 		iconNav: [
 			{ icon: 'ph:house-bold', text: '个人主页', url: blogConfig.author.homepage },
-			{ icon: 'ri:qq-line', text: '交流电台', url: 'https://qm.qq.com/q/lZxfLjrbxu' },
 			{ icon: 'ph:github-logo-bold', text: 'GitHub: Mugzx', url: 'https://github.com/Mugzx' },
 			{ icon: 'ph:rss-simple-bold', text: 'Atom订阅', url: '/atom.xml' },
 			{ icon: 'ph:subway-bold', text: '开往', url: 'https://www.travellings.cn/go.html' },
